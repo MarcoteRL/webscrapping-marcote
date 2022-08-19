6a20d0e1c3d11b2c9ac926702eb5e5948ec91bc6
Marcote <andres@Marcote.localdomain>
2022-08-19 03:23:18 +0200

    Update Bot

--- botAlert.py
+++ botAlert.py
@@ -1,13 +1,37 @@
+#!/usr/bin/env python3.9
+from bs4 import BeautifulSoup
+import telebot
 import requests
-from webscrapping import precioSuspFinal
-from webscrapping import precioDeseado
 
-def telegram_bot_sendtext(bot_message):  
-    bot_token = '5604399517:AAE1l4-SjlgyDbWCy8MV3Sg3WosgVApe_qE'
-    bot_chatID = '1325259916'
-    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
-    response = requests.get(send_text)
-    return response.json()
-    
+url = requests.get("https://www.oscaro.es/juego-de-4-amortiguadores-muelles-koni-1120-3172-4990571-190-p?utm_source=google&utm_medium=cpc&utm_campaign=ES-Shopping-Zombie-PMax&utm_content=ES-Smart-Shopping-Zombie&utm_term=shopping-4990571&gclid=CjwKCAjwo_KXBhAaEiwA2RZ8hExKQ0RcDK-h7dHZ-bTO4pFOvx9j3pr03Yj-q5MHR4PVcf8YU4MgvBoC75oQAvD_BwE#/?vid=19911&vident=bSmUgc3VpcyBEaW9ueXNvc-NnkGH7nIvQKUeSIOTfyxrTwh3hy33oXffsiefYVP8C")
+
+soup = BeautifulSoup(url.content, 'html.parser')
+result = soup.find("p", {"class": "price"})
+precioSuspEuros = result.text
+precioSusp = precioSuspEuros.replace("€", "")
+precioSusp2 = precioSusp.replace(",", ".")
+precioSuspFinal = float(precioSusp2)
+print(result.text)
+precioDeseado = 385.00
+
+
+bot = telebot.TeleBot('5604399517:AAE1l4-SjlgyDbWCy8MV3Sg3WosgVApe_qE', parse_mode = "None")
+
+@bot.message_handler(commands=['help', 'start'])
+
+def enviar(message):
+    bot.reply_to(message, "Hola, soy un bot que1 te avisa cuando el precio de un producto cambia.\nPara usarme escribe /precio")
+
+@bot.message_handler(func=lambda message: "/precio" in message.text)
+
+def enviar_precio(message):
+    bot.reply_to(message, "El precio actual es de " + str(precioSuspFinal) + "€")
+    if precioSuspFinal <= precioDeseado:
+        bot.reply_to(message, f"¡ATENCION! Hay oferta,bajó el precio! Está en: {str(precioSuspFinal) + '€'}\nEnlace: https://cutt.ly/fXzM2xi")
+    else:
+        bot.reply_to(message, f"Está muy caro aún 😢😢")
+
 if precioSuspFinal <= precioDeseado:
-    test = telegram_bot_sendtext(f"¡ATENCION! Hay oferta,bajo el precio! Está en: {str(precioSuspFinal) + '€'}\nEnlace: https://cutt.ly/fXzM2xi")
+    bot.send_message(1325259916, f"¡ATENCION! Hay oferta,bajó el precio! Está en: {str(precioSuspFinal) + '€'}\nEnlace: https://cutt.ly/fXzM2xi")
+
+bot.polling()
