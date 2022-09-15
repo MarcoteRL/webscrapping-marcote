1e2cc0c6fada6667232f808ec66cc14f3dd84ff3
Marcote <andres@Marcote.localdomain>
2022-09-15 15:08:23 +0200

    Update Bot

--- botAlert.py
+++ botAlert.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3.9
+# -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup
+import time
 import telebot
 import requests
 
 url = requests.get("https://www.oscaro.es/juego-de-4-amortiguadores-muelles-koni-1120-3172-4990571-190-p?utm_source=google&utm_medium=cpc&utm_campaign=ES-Shopping-Zombie-PMax&utm_content=ES-Smart-Shopping-Zombie&utm_term=shopping-4990571&gclid=CjwKCAjwo_KXBhAaEiwA2RZ8hExKQ0RcDK-h7dHZ-bTO4pFOvx9j3pr03Yj-q5MHR4PVcf8YU4MgvBoC75oQAvD_BwE#/?vid=19911&vident=bSmUgc3VpcyBEaW9ueXNvc-NnkGH7nIvQKUeSIOTfyxrTwh3hy33oXffsiefYVP8C")
-
 soup = BeautifulSoup(url.content, 'html.parser')
-result = soup.find("p", {"class": "price"})
+result = soup.find('p', class_='price')
 precioSuspEuros = result.text
 precioSusp = precioSuspEuros.replace("€", "")
 precioSusp2 = precioSusp.replace(",", ".")
@@ -15,23 +16,29 @@ print(result.text)
 precioDeseado = 385.00
 
 
-bot = telebot.TeleBot('5604399517:AAE1l4-SjlgyDbWCy8MV3Sg3WosgVApe_qE', parse_mode = "None")
+bot = telebot.TeleBot(
+    '5604399517:AAE1l4-SjlgyDbWCy8MV3Sg3WosgVApe_qE', parse_mode="None")
+
 
 @bot.message_handler(commands=['help', 'start'])
-
 def enviar(message):
-    bot.reply_to(message, "Hola, soy un bot que1 te avisa cuando el precio de un producto cambia.\nPara usarme escribe /precio")
+    bot.reply_to(
+        message, "Hola, soy un bot que1 te avisa cuando el precio de un producto cambia.\nPara usarme escribe /precio")
+
 
 @bot.message_handler(func=lambda message: "/precio" in message.text)
-
 def enviar_precio(message):
-    bot.reply_to(message, "El precio actual es de " + str(precioSuspFinal) + "€")
+    bot.reply_to(message, "El precio actual es de " +
+                 str(precioSuspFinal) + "€")
     if precioSuspFinal <= precioDeseado:
-        bot.reply_to(message, f"¡ATENCION! Hay oferta,bajó el precio! Está en: {str(precioSuspFinal) + '€'}\nEnlace: https://cutt.ly/fXzM2xi")
+        bot.reply_to(
+            message, f"¡ATENCION! Hay oferta,bajó el precio! Está en: {str(precioSuspFinal) + '€'}\nEnlace: https://cutt.ly/fXzM2xi")
     else:
         bot.reply_to(message, f"Está muy caro aún 😢😢")
 
+
 if precioSuspFinal <= precioDeseado:
-    bot.send_message(1325259916, f"¡ATENCION! Hay oferta,bajó el precio! Está en: {str(precioSuspFinal) + '€'}\nEnlace: https://cutt.ly/fXzM2xi")
+    bot.send_message(
+        1325259916, f"¡ATENCION! Hay oferta,bajó el precio! Está en: {str(precioSuspFinal) + '€'}\nEnlace: https://cutt.ly/fXzM2xi")
 
 bot.polling()
